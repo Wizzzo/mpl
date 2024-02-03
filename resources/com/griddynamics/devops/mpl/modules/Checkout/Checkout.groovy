6e515a154323fc3448e4a1b205c363dee99e2012
ori wiesel <oriwiesel@gmail.com>
2024-02-03 17:16:46 +0200

    test

--- resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
@@ -6,21 +6,21 @@ def gitBranch = "master"
 def gitUrl = CFG.'git.url'
 def gitRef = "+refs/heads/${gitBranch}:refs/remotes/origin/${gitBranch}"
 
-echo gitUrl
+echo "gitUrl ${gitUrl}"
 
-// checkout(
-//   changelog: false, 
-//   poll: false, 
-//   scm: [
-//       $class: 'GitSCM', branches: [[name: "master"]], 
-//       doGenerateSubmoduleConfigurations: false, 
-//       extensions: [
-//           [$class: 'CloneOption', honorRefspec: true, noTags: true, reference: '', shallow: true], 
-//           [$class: 'CleanBeforeCheckout'],
-//           ,[$class: 'WipeWorkspace']
-//       ],
-//       userRemoteConfigs: [
-//           [name: 'origin',  refspec: "${}", credentialsId: 'Gtihub-ssh-key', url: "${gitUrl}"]
-//       ]
-//   ]
-// )
+checkout(
+  changelog: false, 
+  poll: false, 
+  scm: [
+      $class: 'GitSCM', branches: [[name: "master"]], 
+      doGenerateSubmoduleConfigurations: false, 
+      extensions: [
+          [$class: 'CloneOption', honorRefspec: true, noTags: true, reference: '', shallow: true], 
+          [$class: 'CleanBeforeCheckout'],
+          ,[$class: 'WipeWorkspace']
+      ],
+      userRemoteConfigs: [
+          [name: 'origin',  refspec: "${}", credentialsId: 'Gtihub-ssh-key', url: "${gitUrl}"]
+      ]
+  ]
+)
