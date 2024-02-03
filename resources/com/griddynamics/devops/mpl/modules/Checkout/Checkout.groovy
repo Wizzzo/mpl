8817846296eb27eeef3cfcef4b64cfae41bdc6f2
ori wiesel <oriwiesel@gmail.com>
2024-02-03 14:40:59 +0200

    add checkout

--- resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
@@ -2,7 +2,23 @@
  * Common checkout module
  */
 
-if( CFG.'git.url' )
-  MPLModule('Git Checkout', CFG)
-else
-  MPLModule('Default Checkout', CFG)
+def gitBranch = "master"
+def gitUrl = CFG."git.url"
+def gitRef = "+refs/heads/${gitBranch}:refs/remotes/origin/${gitBranch}"
+
+checkout(
+  changelog: false, 
+  poll: false, 
+  scm: [
+      $class: 'GitSCM', branches: [[name: "master"]], 
+      doGenerateSubmoduleConfigurations: false, 
+      extensions: [
+          [$class: 'CloneOption', honorRefspec: true, noTags: true, reference: '', shallow: true], 
+          [$class: 'CleanBeforeCheckout'],
+          ,[$class: 'WipeWorkspace']
+      ],
+      userRemoteConfigs: [
+          [name: 'origin',  refspec: "${}", credentialsId: 'Gtihub-ssh-key', url: "${gitUrl}"]
+      ]
+  ]
+)
